f84fbdc19a058a584217d0417c7b58bacd7431a2
rurounim <mmirziashvili10@gmail.com>
2024-12-19 17:07:41 +0400

    initial

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,34 @@
+
+package base;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BaseTest {
+
+    @BeforeClass
+    public void setUp() {
+        Configuration.baseUrl = "https://demo.automationtesting.in/Register.html";
+        Configuration.timeout = 10000;
+        Configuration.browser = "firefox"; // Change this configuration to use a different browser
+        open(Configuration.baseUrl);
+
+        String url = WebDriverRunner.url();
+        assert(url).equals(Configuration.baseUrl);
+
+    }
+
+    @AfterMethod
+    public void refreshWindow() {
+        refresh();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/tests/RegisterTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import base.BaseTest;
+import io.qameta.allure.Description;
+import org.testng.annotations.Test;
+import pages.RegisterPage;
+
+import com.codeborne.selenide.Condition;
+
+public class RegisterTest extends BaseTest {
+
+
+}
